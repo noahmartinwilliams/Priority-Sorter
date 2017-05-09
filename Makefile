946640e9465eed13806deb67ef16be97b851e8f2
skynet1 <thecrazymancan@gmail.com>
2017-05-08 19:30:34 -0500

    Added man page and error checking to see if input file exists.

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ HC=ghc --make $@
 .PHONY: clean install
 install: Prioritizer
 	install Prioritizer /usr/bin/prioritizer
+	install prioritizer.1 /usr/share/man/man1/prioritizer.1
 
 Prioritizer: Prioritizer.hs
 	$(HC)
--- Prioritizer.hs
+++ Prioritizer.hs
@@ -1,13 +1,27 @@
 module Main where
 
+import System.Directory
 import System.IO
 import Data.List.Split
 import Data.Either
 import System.Environment
 import System.IO.Unsafe
 
+compareGEIO :: Show a => a -> a -> IO Bool
+compareGEIO a b = do
+	putStrLn ("\n" ++ (show a) ++ ">=" ++ (show b) ++ " [y/Y/n/N] ? ") 
+	l <- getLine 
+	if l == "y" || l == "Y"
+	then
+		return True
+	else if l == "n" || l == "N"
+	then
+		return False
+	else
+		compareGEIO a b
+
 compareGE :: Show a => a -> a -> Bool
-compareGE a b = unsafePerformIO (do putStrLn ("\n" ++ (show a) ++ " >= " ++ (show b) ++ " ? ") ; l <- getLine ; if l == "y" || l == "Y" then return True else return False)
+compareGE a b = unsafePerformIO (compareGEIO a b)
 
 filter2 :: (a -> Bool) -> [a] -> ([a], [a])
 filter2 func list = let divided tmp = if func tmp then Left tmp else Right tmp in
@@ -29,6 +43,10 @@ main = do
 		putStrLn "Usage: Sorter input-file output-file"
 	else do
 		let (input : output : _) = args in do
-			i <- readFile input
-			let lines = (endBy "\n" i) in
-				writeFile output (foldr (\x -> \y -> x ++ "\n" ++ y) "" (sort lines))
+			if (unsafePerformIO (doesFileExist input)) == False
+			then
+				putStrLn ("error: " ++ (show input) ++ " does not exist.") 
+			else do
+				i <- readFile input
+				let lines = (endBy "\n" i) in
+					writeFile output (foldr (\x -> \y -> x ++ "\n" ++ y) "" (sort lines))
